e1120c8e8a981de618a2310c73580b629f10a44b
Ahmedsultan2021 <ahmedsultan4277892@gmail.com>
2024-04-20 00:05:06 +0200

    first commit

---
+++ resources/js/Pages/admins/home.vue
@@ -0,0 +1,14 @@
+<template>
+    <AdminsLayout>
+        hi
+    </AdminsLayout>
+</template>
+
+<script setup>
+import AdminsLayout from '@/Layouts/AdminsLayout.vue';
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
